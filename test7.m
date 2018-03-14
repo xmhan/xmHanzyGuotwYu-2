c4b0d680e797f4b4158c8aac6ae3280096f3e55b
evelynzhiyi <36683353+evelynzhiyi@users.noreply.github.com>
2018-03-13 20:20:58 -0700

    Add files via upload

---
+++ test7.m
@@ -0,0 +1,82 @@
+% The parameters of neural networks are the numbers of layers, the
+% number of neurons per layer, and the size of the training set.
+% As the number of layers increases, the error decreases.
+% As the number of neurons per layer increases, the error decreases.
+% As the size of the training set increases, the error decreases.
+% ERROR(less layer) =
+
+    0.1000
+    0.1000
+    0.0909
+    0.1000
+    0.0833
+    0.0909
+    0.0833
+    0.0769
+    0.0833
+    0.9091
+    
+% ERROR(more layer) =
+
+    0.0833
+    0.0833
+    0.0714
+    0.0769
+    0.0833
+    0.1000
+    0.0833
+    0.0769
+    0.0769
+    0.8889
+    
+% ERROR(less neuron) =
+
+    0.0833
+    0.1000
+    0.1000
+    0.0769
+    0.0769
+    0.0833
+    0.0833
+    0.0769
+    0.0833
+    0.9167
+    
+% ERROR(more neuron) =
+
+    0.0625
+    0.0667
+    0.0909
+    0.0667
+    0.0667
+    0.0625
+    0.0588
+    0.0625
+    0.0625
+    0.0625
+ 
+% ERROR(small training set) =
+
+    0.1000
+    0.0833
+    0.0769
+    0.0833
+    0.1000
+    0.0833
+    0.0909
+    0.1000
+    0.1111
+    0.9286
+    
+% ERROR(large training set) =
+
+    0.0833
+    0.0769
+    0.0678
+    0.0769
+    0.0833
+    0.0769
+    0.0833
+    0.0909
+    0.1111
+    0.8750
